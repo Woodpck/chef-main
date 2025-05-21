4b780360a77d84fa48373db69d5d22405596b35e
TigiDots <dotonronald3@gmail.com>
2025-05-21 15:15:50 +0800

    fix comment

--- LexicalAnalyzer.py
+++ LexicalAnalyzer.py
@@ -1,5 +1,5 @@
 # CONSTANTS
-EOF = '/'
+EOF = '|'
 
 ALPHA_BIG = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 ALPHA_SMALL = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
